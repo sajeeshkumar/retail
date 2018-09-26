cb7c4d453e684b28d89480960e50e0968c746f8a
Sajeesh Kumar <sajeesh.kumar@gmail.com>
2018-09-26 14:08:49 -0400

    First commit

--- order/src/main/java/net/sajeesh/retail/order/persistence/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package net.sajeesh.retail.order.persistence;
-
-import net.sajeesh.retail.order.domain.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, String> {
-}
